fe3baeb62b4e9a5b78eceacb3ee36760ef8b61b7
Alexandre <Alexandre.Lorbeer@web.de>
2019-05-09 23:21:31 +0200

    refactor(): added click navigation

--- src/app/features/landing/components/home/home.component.ts
+++ src/app/features/landing/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'esi-home',
@@ -7,16 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  cards: { title: string, icon: string, color: string }[] = [
-    { title: 'Produktions-überwachung', icon: 'format_list_numbered', color: 'lightblue' },
-    { title: 'Fertigungs-aufträge', icon: 'format_color_fill', color: 'lightgreen' },
-    { title: 'Kennzahlen', icon: 'insert_chart', color: 'lightsalmon' }
+  cards: { title: string, icon: string, color: string, route: string }[] = [
+    { title: 'Produktions-überwachung', icon: 'format_list_numbered', color: 'lightblue', route: 'visualisation/monitoring' },
+    { title: 'Fertigungs-aufträge', icon: 'format_color_fill', color: 'lightgreen', route: 'visualisation/orders' },
+    { title: 'Kennzahlen', icon: 'insert_chart', color: 'lightsalmon', route: 'reporting/keyfigures' }
   ]
-  
-  constructor() { }
+
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
 
+  }
+
+  navigate(route: string){
+    this._router.navigate([route]);
   }
 
 }
