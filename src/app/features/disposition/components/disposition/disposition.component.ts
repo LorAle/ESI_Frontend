6735adb861cf12aa8aba400329b4f8ddca7342ea
Benjamin Seifert <bseifert@stud.hs-offenburg.de>
2019-05-15 13:15:13 +0200

    Adding Navigation Cards

--- src/app/features/disposition/components/disposition/disposition.component.ts
+++ src/app/features/disposition/components/disposition/disposition.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'esi-disposition',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DispositionComponent implements OnInit {
 
-  constructor() { }
+  cards: { title: string, icon: string, color: string, route: string }[] = [
+    { title: 'Bestellung', icon: 'format_list_numbered', color: 'lightblue', route: 'disposition/order' },
+    { title: 'Einlagerung', icon: 'format_list_numbered', color: 'lightyellow', route: 'disposition/storage' },
+  ]
+
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  navigate(route: string){
+    this._router.navigate([route]);
   }
 
 }
