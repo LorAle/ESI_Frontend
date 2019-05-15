603b23b7adaf17d0c8d080b99b594c742fef7877
Benjamin Seifert <bseifert@stud.hs-offenburg.de>
2019-05-15 13:07:29 +0200

    adding the order and storage container

--- src/app/features/disposition/disposition-routing.module.ts
+++ src/app/features/disposition/disposition-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DispositionComponent } from './components';
+import { DispositionComponent } from './components/disposition/disposition.component';
+import { OrderComponent, StorageComponent } from './containers';
 
 const routes: Routes = [
-  { path: 'disposition', component: DispositionComponent }
+  { path: 'disposition', component: DispositionComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'storage', component: StorageComponent }
 ];
 
 @NgModule({
--- src/app/features/disposition/disposition.module.ts
+++ src/app/features/disposition/disposition.module.ts
@@ -5,9 +5,11 @@ import { DispositionRoutingModule } from './disposition-routing.module';
 import { DispositionComponent } from './components/disposition/disposition.component';
 import { SharedModule } from '../../shared/shared.module';
 import { CoreModule } from '../../core/core.module';
+import { OrderComponent } from './containers/order/order.component';
+import { StorageComponent } from './containers/storage/storage.component';
 
 @NgModule({
-  declarations: [DispositionComponent],
+  declarations: [DispositionComponent, OrderComponent, StorageComponent],
   imports: [
     DispositionRoutingModule,
     CommonModule,
