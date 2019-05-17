298edf921f471d1266b51c3410a69d75130c972d
Alexandre <Alexandre.Lorbeer@web.de>
2019-05-17 12:07:54 +0200

    impl: added methods for mawi

--- src/app/core/services/mawi.service.ts
+++ src/app/core/services/mawi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,20 @@ export class MawiService {
   ) {
     this.baseUrl = environment.baseUrl + '/mawi';
   }
-  
-  jsonHeader(){
+
+  jsonHeader() {
     return new HttpHeaders({ 'Content-Type': 'application/json' });
+  }
+
+  supplyMaterial(data: { Typ: string, Menge: number }): Observable<any> {
+    return this._http.post<any>(this.baseUrl + `/order/supply`, JSON.stringify(data), {
+      headers: this.jsonHeader()
+    });
+  }
+
+  collectMaterial(data: { Material: string, Menge: number, Fertigungsauftrag: number, Auftragsnummer: number }): Observable<any> {
+    return this._http.post<any>(this.baseUrl + `/order/supply`, JSON.stringify(data), {
+      headers: this.jsonHeader()
+    });
   }
 }
